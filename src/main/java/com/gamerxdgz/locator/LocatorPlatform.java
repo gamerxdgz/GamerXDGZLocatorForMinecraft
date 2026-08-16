af287cf145f052df3cae2816116b504ca01c9ed2
GamerXD_GZ <singhgitansh741@gmail.com>
2026-08-16 13:09:28 -0700

    Update LocatorPlatform.java

--- src/main/java/com/gamerxdgz/locator/LocatorPlatform.java
+++ src/main/java/com/gamerxdgz/locator/LocatorPlatform.java
@@ -1,5 +1,7 @@
 package com.gamerxdgz.locator;
 
+import org.bukkit.entity.Player;
+
 public interface LocatorPlatform {
 
     String getPlatformName();
@@ -7,4 +9,11 @@ public interface LocatorPlatform {
     boolean supportsActionBar();
 
     boolean supportsLegacyClients();
+
+    void sendLocator(
+            Player player,
+            String message
+    );
+
+    void clearLocator(Player player);
 }

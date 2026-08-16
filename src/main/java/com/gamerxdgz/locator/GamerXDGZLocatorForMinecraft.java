aa3c91a0e5cdd40f6d220e41cfc784b62c22a2cb
GamerXD_GZ <singhgitansh741@gmail.com>
2026-08-16 12:09:15 -0700

    Connect locator manager to plugin

--- src/main/java/com/gamerxdgz/locator/GamerXDGZLocatorForMinecraft.java
+++ src/main/java/com/gamerxdgz/locator/GamerXDGZLocatorForMinecraft.java
@@ -4,13 +4,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GamerXDGZLocatorForMinecraft extends JavaPlugin {
 
+    private LocatorManager locatorManager;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        locatorManager = new LocatorManager(this);
+
         getLogger().info("GamerXDGZLocatorForMinecraft has been enabled!");
     }
 
     @Override
     public void onDisable() {
+        locatorManager = null;
+
         getLogger().info("GamerXDGZLocatorForMinecraft has been disabled!");
+    }
+
+    public LocatorManager getLocatorManager() {
+        return locatorManager;
     }
 }
